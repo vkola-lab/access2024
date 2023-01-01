52cf4e18bd0891e3b99cd5d0acc0b9a3a3ed8b03
mfromano <Michael Romano>
2023-01-01 17:59:33 -0500

    updated matrix stats

--- plot/matrix_stat.py
+++ plot/matrix_stat.py
@@ -1,8 +1,14 @@
-from utils_stat import get_roc_info, get_pr_info, calc_neurologist_statistics, read_raw_score
+from utils_stat import (
+    get_roc_info,
+    get_pr_info,
+    calc_neurologist_statistics,
+    read_raw_score,
+)
 import matplotlib.pyplot as plt
 from utils_plot import plot_curve, plot_legend, plot_neorologist
 from time import time
 import numpy as np
+
 
 def confusion_matrix(labels, scores):
     matrix = [[0, 0], [0, 0]]
@@ -26,8 +32,10 @@ def get_metrics(matrix):
     ACCU = (TP + TN) / (TP + TN + FP + FN)
     Sens = TP / (TP + FN + 0.0000001)
     Spec = TN / (TN + FP + 0.0000001)
-    F1 = 2*TP/(2*TP+FP+FN)
-    MCC = (TP*TN-FP*FN)/((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN)+0.00000001)**0.5
+    F1 = 2 * TP / (2 * TP + FP + FN)
+    MCC = (TP * TN - FP * FN) / (
+        (TP + FP) * (TP + FN) * (TN + FP) * (TN + FN) + 0.00000001
+    ) ** 0.5
     return ACCU, Sens, Spec, F1, MCC
 
 
@@ -44,26 +52,41 @@ def stat_metric(matrices):
     # print('Sens {0:.4f}+/-{1:.4f}'.format(float(np.mean(Sens)), float(np.std(Sens))))
     # print('Spec {0:.4f}+/-{1:.4f}'.format(float(np.mean(Spec)), float(np.std(Spec))))
     # print('F1   {0:.4f}+/-{1:.4f}'.format(float(np.mean(F1)),   float(np.std(F1))))
-    MCC_text = 'MCC  {0:.4f}+/-{1:.4f}'.format(float(np.mean(MCC)), float(np.std(MCC)))
-    group = (float(np.mean(Accu)), float(np.std(Accu)), float(np.mean(Sens)), float(np.std(Sens)), float(np.mean(Spec)), \
-    float(np.std(Spec)), float(np.mean(F1)),   float(np.std(F1)), float(np.mean(MCC)), float(np.std(MCC)))
+    MCC_text = "MCC  {0:.4f}+/-{1:.4f}".format(float(np.mean(MCC)), float(np.std(MCC)))
+    group = (
+        float(np.mean(Accu)),
+        float(np.std(Accu)),
+        float(np.mean(Sens)),
+        float(np.std(Sens)),
+        float(np.mean(Spec)),
+        float(np.std(Spec)),
+        float(np.mean(F1)),
+        float(np.std(F1)),
+        float(np.mean(MCC)),
+        float(np.std(MCC)),
+    )
     return group, MCC_text
 
 
 def compute_mlp_matrix_stat():
-    Matrix = []
-    for dataset in ('NACC','ADNI',):
-        if dataset == 'NACC':
-            stage = 'all'
-        elif dataset == 'ADNI':
-            stage = 'test'
+    for dataset in (
+        "NACC",
+        "ADNI",
+    ):
+        if dataset == "NACC":
+            stage = "all"
+        elif dataset == "ADNI":
+            stage = "test"
         else:
-            raise 'NotImplementedError'
+            raise NotImplementedError
         Matrix = []
-        for exp in range(5): 
-            labels, scores = read_raw_score(f'../checkpoint_dir/mlp_bce_{exp}_exp{exp}/raw_score_{dataset}_{exp}_{stage}.txt')
+        for exp in range(5):
+            labels, scores = read_raw_score(
+                f"../checkpoint_dir/mlp_bce_{exp}_exp{exp}/raw_score_{dataset}_{exp}_{stage}.txt"
+            )
             Matrix.append(confusion_matrix(labels, scores))
         print(stat_metric(Matrix))
+
 
 if __name__ == "__main__":
     # names = ['T', 'Z', 'G', 'CG_1']
@@ -80,7 +103,3 @@ if __name__ == "__main__":
     #         _, txt = stat_metric(Matrix)
     #         print(n, d, txt)
     compute_mlp_matrix_stat()
-
-
-
-
